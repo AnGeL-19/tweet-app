b9b26e0e43828ae3406f54c47c24485ae0c96bb2
AngelMR <mra7588@gmail.com>
2024-09-04 18:07:17 -0600

    added env

--- src/adapters/http/api.ts
+++ src/adapters/http/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-
+const apiUrl = import.meta.env.VITE_API_URL;
 // 'https://app-tweet-backend.vercel.app/api/'
 
 export const tweetApi = axios.create({
-    baseURL: 'http://localhost:8080/api/',
+    baseURL: apiUrl,
     withCredentials: true
 })
   
