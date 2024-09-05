8e7f4b46c9d5463b9a11374c151800f608352d22
AngelMR <mra7588@gmail.com>
2024-09-04 19:32:11 -0600

    fix: img loading

--- src/app/components/shared/LoadingPage.tsx
+++ src/app/components/shared/LoadingPage.tsx
@@ -2,7 +2,7 @@
 export const LoadingPage = () => {
   return (
     <div className='w-full min-h-screen flex justify-center items-center'>
-      <img className='flex h-20 w-20 animate-in' src="/src/assets/static/tweeter-small.svg" alt="Tweeter Only" />
+      <img className='flex h-20 w-20 animate-in' src="/assets/static/tweeter-small.svg" alt="Tweeter Only" />
     </div>
   )
 }
