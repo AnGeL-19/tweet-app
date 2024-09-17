10b963c6e46dba316896a39b9980f7cb3c2a78f9
AngelMR <mra7588@gmail.com>
2024-09-17 15:14:00 -0600

    fix: changed trandport to polling

--- src/adapters/http/socket.ts
+++ src/adapters/http/socket.ts
@@ -4,6 +4,7 @@ const apiUrlSocket = import.meta.env.VITE_API_URL_SOCKET;
 const token = window.localStorage.getItem('token') || ''
 
 export const socket =  io(apiUrlSocket,{
+    transports: ['polling'],
     autoConnect: false,
     withCredentials: true, // Asegúrate de enviar las credenciales
     extraHeaders: {
